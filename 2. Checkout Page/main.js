ce6d7af0205730f22147e13f8aa9021b16423223
snowleo208 <13027432+snowleo208@users.noreply.github.com>
2018-08-27 21:26:15 +0800

    add: desktop version of checkout page and draft of animation for checkout popup

--- 2. Checkout Page/main.js
+++ 2. Checkout Page/main.js
@@ -1,20 +1,28 @@
 (() => {
-    var toggle = false;
+  var toggle = false;
+    
+  function expandGrid(e) {
+    e.preventDefault();
+    toggle = !toggle;
+    if(toggle) {
+      //document.querySelector('.c-checkout').style.display = "block";
+      document.querySelector('.c-checkout').classList.add('active');
+      document.querySelector('.c-checkout').classList.remove('deactive');
+      document.querySelector('.c-checkout').setAttribute('aria-hidden', false);
+  
+      document.querySelector('.active').addEventListener('animationend', e => e.target.style.zIndex = '1');
       
-    function expandGrid(e) {
-      e.preventDefault();
-      toggle = !toggle;
-      if(toggle) {
-        document.querySelector('.c-checkout').style.display = "block";
-        document.querySelector('.c-checkout').setAttribute('aria-hidden', false);
-      } else {
-        document.querySelector('.c-checkout').style.display = "none";
-        document.querySelector('.c-checkout').setAttribute('aria-hidden', true);
-      }
+    } else {
+      //document.querySelector('.c-checkout').style.display = "none";
+      document.querySelector('.c-checkout').style.zIndex = null;
+      document.querySelector('.c-checkout').classList.remove('active');
+      document.querySelector('.c-checkout').classList.add('deactive');
+      document.querySelector('.c-checkout').setAttribute('aria-hidden', true);
     }
-    
-    document.querySelector('.c-checkout--submit').addEventListener('click', e => e.preventDefault());
-    
-    document.querySelector('.c-product--cart').addEventListener('click',expandGrid);
-    
-    })()
+  }
+  
+  document.querySelector('.c-checkout--submit').addEventListener('click', e => e.preventDefault());
+  
+  document.querySelector('.c-product--cart').addEventListener('click',expandGrid);
+  
+  })()
--- log.md
+++ log.md
@@ -7,3 +7,11 @@
 **Thoughts:** I really struggled with the animation, hopefully I can find a better way to present the state changes.
 
 **Link to work:** [Login Page](https://codepen.io/snowleo208/pen/XPXjeQ)
+
+### Day 1: 26 Aug 2018
+
+**Today's Progress**: Created second item: checkout page.
+
+**Thoughts:** Finished draft of mobile version and basic function of shopping cart button. Planned to implement processing order animation, this is my first time to do this type of animation, I guess may need more time to plan and create.
+
+**Link to work:** [Checkout Page](https://codepen.io/snowleo208/pen/dqGRxx) (Draft only)
